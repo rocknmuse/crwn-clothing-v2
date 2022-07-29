9f9c13aa838f54c26e021e2d7e35dabc14ba9f6e
Inna Baikova <ivshatova@gmail.com>
2022-07-29 14:16:57 +0300

    Button component migration

--- src/store/user/user.slice.ts
+++ src/store/user/user.slice.ts
@@ -1,6 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { RootState } from '../root-reducer';
-import { UserState } from './user.types';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User } from 'firebase/auth'
+import { UserData } from '../../utils/firebase/firebase.utils'
+import { RootState } from '../root-reducer'
+import { UserState } from './user.types'
 
 const initialState: UserState = {
     currentUser: null
@@ -10,13 +12,13 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setCurrentUser: (state, action) => {
+        setCurrentUser: (state, action: PayloadAction<UserData | null>) => {
             state.currentUser = action.payload
         }
     }
 })
 
-export const selectCurrentUser = (state: RootState) => state.user.currentUser;
+export const selectCurrentUser = (state: RootState) => state.user.currentUser
 
 export const { setCurrentUser } = userSlice.actions
 export default userSlice.reducer
